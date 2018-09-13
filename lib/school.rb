a618ef46056d633a17575eb770da992daa8f370e
Heather Miller <heather.miller114@gmail.com>
2018-09-13 17:50:41 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
 # code here!
+class School
+  attr_accessor :name
+  
+  def initialize(roster)
+    roster = {}
+    @roster = roster
+  end
+  
+  def roster
+    if roster.length != 0
+      @roster.length
+    end
+  end
+  
+end
